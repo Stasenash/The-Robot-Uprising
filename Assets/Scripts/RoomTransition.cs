6f83bdedd94771e36b05d61381bbe02ab68fcb05
StriderAJR <alexander.a.kirsanov@gmail.com>
2024-01-27 22:54:18 +0500

    room trans

--- Assets/Scripts/RoomTransition.cs
+++ Assets/Scripts/RoomTransition.cs
@@ -2,17 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RoomTransition : MonoBehaviour
 {
     public GameObject currentCamera;
     public GameObject newCamera;
 
+    private NavMeshAgent nva;
+
+    private GameObject robot;
+
     // Start is called before the first frame update
     void Start()
     {
-
-	}
+        robot = GameObject.Find("Banana Man");
+        nva = robot.GetComponent<NavMeshAgent>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,10 +30,22 @@ public class RoomTransition : MonoBehaviour
 	{
         string[] name = gameObject.name.Split(' ');
 
-        if (name.Length == 2)
+        //if (name.Length == 2)
+        //{
+        //    Debug.Log(name[0]);
+        //    return;
+        //}
+
+        switch (name[1])
         {
-            Debug.Log(name[0]);
-            return;
+            case "Bathroom":
+                nva.enabled = false;
+                robot.transform.position = new Vector3(-903.0f, 0.1f, 170.0f); nva.enabled = true;
+                break;
+            case "MainRoom": nva.enabled = false; robot.transform.position = new Vector3(6.0f, 0.1f, 5.2f); nva.enabled = true; break;
+            case "Kitchen": nva.enabled = false; robot.transform.position = new Vector3(39.0f, 0.1f, -425.0f); nva.enabled = true; ; break;
+            case "Bedroom": nva.enabled = false; robot.transform.position = new Vector3(230.0f, 0.1f, 158.0f); nva.enabled = true; ; break;
+            case "Door":;break;//переход на катсцену ивентов при условии
         }
 
         currentCamera.SetActive(false);
