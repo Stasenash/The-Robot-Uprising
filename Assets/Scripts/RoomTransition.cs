f87f1f1ab4bd9f8e6b9be101e7608a71cfb62104
StriderAJR <alexander.a.kirsanov@gmail.com>
2024-01-27 23:30:56 +0500

    scaling robot

--- Assets/Scripts/RoomTransition.cs
+++ Assets/Scripts/RoomTransition.cs
@@ -42,9 +42,36 @@ public class RoomTransition : MonoBehaviour
                 nva.enabled = false;
                 robot.transform.position = new Vector3(-903.0f, 0.1f, 170.0f); nva.enabled = true;
                 break;
-            case "MainRoom": nva.enabled = false; robot.transform.position = new Vector3(6.0f, 0.1f, 5.2f); nva.enabled = true; break;
-            case "Kitchen": nva.enabled = false; robot.transform.position = new Vector3(39.0f, 0.1f, -425.0f); nva.enabled = true; ; break;
-            case "Bedroom": nva.enabled = false; robot.transform.position = new Vector3(230.0f, 0.1f, 158.0f); nva.enabled = true; ; break;
+            case "MainRoom":
+                nva.enabled = false;
+                switch (name[0])
+                {
+                    case "Bathroom":
+                        robot.transform.position = new Vector3(-7.6f, 0.1f, 5.1f);
+                        break;
+                    case "Bedroom":
+                        robot.transform.position = new Vector3(6.0f, 0.1f, 5.2f);
+                        robot.transform.localScale /= 1.3f;
+                        break;
+                    case "Kitchen":
+                        robot.transform.position = new Vector3(7.6f, 0.1f, -4.7f);
+                        robot.transform.localScale /= 1.7f;
+                        break;
+                }
+                nva.enabled = true;
+                break;
+            case "Kitchen":
+                nva.enabled = false;
+                robot.transform.position = new Vector3(39.0f, 0.1f, -425.0f);
+                robot.transform.localScale *= 1.7f;
+                nva.enabled = true;
+                break;
+            case "Bedroom":
+                nva.enabled = false;
+                robot.transform.position = new Vector3(230.0f, 0.1f, 158.0f);
+                robot.transform.localScale *= 1.3f; 
+                nva.enabled = true;
+                break;
             case "Door":;break;//переход на катсцену ивентов при условии
         }
 
